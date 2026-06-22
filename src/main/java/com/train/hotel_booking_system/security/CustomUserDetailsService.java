e85169fe1f14a43fc478423345001df3dd36570e
Mohamed Hendy <midohendy48@gmail.com>
2026-06-22 19:08:35 +0300

    chore: add custom user details service

---
+++ src/main/java/com/train/hotel_booking_system/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.train.hotel_booking_system.security;
+
+import com.train.hotel_booking_system.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        com.train.hotel_booking_system.entity.User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+}
